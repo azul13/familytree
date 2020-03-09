bbe6e8c1c57140f7806b36ada1d821449ae66a2b
azul13 <49488788+azul13@users.noreply.github.com>
2020-03-09 16:57:57 -0500

    update familytree.js

--- src/container/components/FamilyTree.js
+++ src/container/components/FamilyTree.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3'
-import graph from './graph2'
+import graph from './obamatree'
 import './FamilyTree.css';
 
 export default() =>  {
